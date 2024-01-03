c8e53e1e8836802cd914938f709e353e997d7144
Ashish Shedage <ashish@Ashishs-MacBook-Air.local>
2024-01-03 21:44:58 +0530

    handled large file

--- src/componnets/Home/Promo.jsx
+++ src/componnets/Home/Promo.jsx
@@ -1,6 +1,5 @@
 import React, {useRef} from 'react';
 import ReactPlayer from 'react-player';
-import { promo } from '../../assets';
 import { useInView } from 'framer-motion';
 
 
@@ -11,7 +10,7 @@ function Promo() {
     <div ref={container} className='p-[64px] h-[110vh] flex items-center bg-black'>
         <div style={{opacity: isInView ? 1 : 0 , transition: "all 3s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"}} className='w-full h-[673px] border-4 border-primary rounded-2xl mt-20'>
             <video className='object-cover rounded-xl h-full w-full' muted autoPlay loop>
-                <source src={promo} type='video/mp4' />
+                <source src={''} type='video/mp4' />
             </video>
         </div>
     </div>
