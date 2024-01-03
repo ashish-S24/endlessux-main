96d79c84ab864d8e5156fab8f7517156b58ffe1c
Ashish Shedage <ashish@Ashishs-MacBook-Air.local>
2024-01-03 21:53:44 +0530

    updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import arrow from './arrow.png';
 import arro_2 from './arrow_2.png';
 import logo from './logo.svg';
 import point_arrow from './Arrow-1-1.svg';
-import promo from './promo.mp4';
+
 import rotate_ele from './rotating_ele.svg';
 import insta from './insta.png';
 import linkedIn_icon from './linkedIn.png';
@@ -12,7 +12,6 @@ export {
     arro_2,
     logo,
     point_arrow,
-    promo,
     rotate_ele,
     insta,
     linkedIn_icon
